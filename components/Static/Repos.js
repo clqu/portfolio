5dfe1c67645aebb7d2c37baaee8dd5e72228d5c8
clqu <76551996+clqu@users.noreply.github.com>
2021-10-14 22:20:11 +0300

    Update Repos.js

--- components/Static/Repos.js
+++ components/Static/Repos.js
@@ -13,7 +13,7 @@ const Repos = () => {
     {db.filter(a => a.stargazers_count > 0).sort((a,b) => b.stargazers_count - a.stargazers_count).map(repo => (
     
         <a href={`https://github.com/${repo.full_name}`} className="w-full p-4 bg-base-200 shadow-lg rounded-md transform transition-all duration-200 hover:-translate-y-1.5 text-sm">
-        {repo.name}
+        {repo.name}<span className="text-gray-300 text-xs ml-2">{repo.full_name}</span>
         <div className="right-0 bottom-0">
         <i className="fal fa-star" />&nbsp;{repo.stargazers_count}
         <i className="fal fa-code-branch ml-2" />&nbsp;{repo.forks_count}
