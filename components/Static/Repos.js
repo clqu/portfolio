fcf0f3e8e8aaf04058c9f31fc6d116ceac517c8a
clqu <76551996+clqu@users.noreply.github.com>
2021-10-14 22:23:42 +0300

    Update Repos.js

--- components/Static/Repos.js
+++ components/Static/Repos.js
@@ -15,9 +15,6 @@ const Repos = () => {
         <a href={`https://github.com/${repo.full_name}`} className="w-full p-4 bg-base-200 shadow-lg rounded-md transform transition-all duration-200 hover:-translate-y-1.5 text-sm">
         {repo.name}
         <div className="right-0 bottom-0">
-        <div>
-        <span className="text-gray-300 text-xs ml-2">{repo.full_name}</span>
-        </div>
         <i className="fal fa-star" />&nbsp;{repo.stargazers_count}
         <i className="fal fa-code-branch ml-2" />&nbsp;{repo.forks_count}
         </div>
