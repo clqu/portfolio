63bdc8c57cb69bcca00b9eca87f3eef7e00e033a
clqu <76551996+clqu@users.noreply.github.com>
2021-10-15 00:08:12 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
             <title>clqu</title>
             <link href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css" rel="stylesheet" />
         </head>
+    <html data-theme="dark">
         <div className="bg-base-300">
             <div className="max-w-screen-lg p-5 w-full md:py-10 md:w-10/12 lg:py-20 lg:w-8/12 mx-auto transition-all duration-300">
                 <Navbar />
@@ -16,6 +17,7 @@ function MyApp({ Component, pageProps }) {
                 <Footer />
             </div>
         </div>
+    </html>
     </>
 }
 
