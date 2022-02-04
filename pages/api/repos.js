3587bf88f4008af59afd8a007464365b10a4457e
clqu <76551996+clqu@users.noreply.github.com>
2022-02-05 02:31:07 +0300

    Update repos.js

--- pages/api/repos.js
+++ pages/api/repos.js
@@ -3,15 +3,18 @@ const fetch = require('isomorphic-unfetch')
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/clqu/repos', {
         headers: {
-            'Authorization': 'token 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'
+            'Authorization': 'token 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'
         }
     })).json();
 
     let __ = await (await fetch('https://api.github.com/users/vcodes-xyz/repos', {
         headers: {
-            'Authorization': 'token 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'
+            'Authorization': 'token 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'
         }
     })).json();
-
-    res.send([..._, ...__])
+    try {
+        res.send([..._, ...__])
+    } catch {
+        res.send([])
+    }
 }
