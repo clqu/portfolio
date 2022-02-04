99e758e24dd44f917206eff589883acb525e1c6c
clqu <76551996+clqu@users.noreply.github.com>
2022-02-05 02:41:33 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
             <link 
-                href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic&display=optional" 
-                rel="stylesheet" 
-            />
+              href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" 
+              rel="stylesheet" 
+          />
         </Head>
         <body>
           <Main />
