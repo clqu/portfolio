a67cf044d4fa5ab5870a07dcec45cf2d21fc422a
clqu <76551996+clqu@users.noreply.github.com>
2022-02-05 02:12:31 +0300

    Delete pages directory

--- pages/api/repos.js
+++
@@ -1,8 +0,0 @@
-const fetch = require('isomorphic-unfetch')
-
-export default async(req,res) => {
-let clqu_repos = await fetch('https://api.github.com/users/clqu/repos').then(res => res.json())
-let vcodes_repos = await fetch('https://api.github.com/users/vcodes-xyz/repos').then(res => res.json())
-
-res.send([...clqu_repos, ...vcodes_repos])
-}
--- pages/api/spotify.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('isomorphic-unfetch')
- 
-export default async(req,res) => {
-let discord_clqu = await fetch('https://api.lanyard.rest/v1/users/714451348212678658').then(res => res.json())
-res.send(discord_clqu)
-}
