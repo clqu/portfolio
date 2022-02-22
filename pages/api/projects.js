06826c1bbe83008249ec7ef55ea57741f3c00d81
clqu <76551996+clqu@users.noreply.github.com>
2022-02-22 11:37:56 +0300

    Update projects.js

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -2,7 +2,7 @@ export default async (req, res) => {
   const data = [
     {
       id: 1,
-      image: "https://i.imgur.com/QgAxBkp.png",
+      image: "https://i.imgur.com/2XwKCNZ.png",
       name: "vCodes",
       description:
         "Do you want to expand and improve your Discord bot? Here vCodes are here for you!",
@@ -31,22 +31,23 @@ export default async (req, res) => {
     },
     {
       id: 5,
-      image: "https://i.imgur.com/oDLp2nF.png",
-      name: "DevsForum",
+      image: "https://i.imgur.com/Qv83QL8.png",
+      name: "Serity",
       description:
-        "DevsForum was opened to provide a benefit for developers and coders, friends.",
-      link: "https://devsforum.net",
-      language: "PHP",
-      languageIcon: "./assets/techs/php.svg",
+        "By creating advanced giveaways with Giftix, you can improve your social media accounts and gain organic lift!",
+      link: "https://giftix.gg",
+      language: "Next.js",
+      languageIcon: "./assets/techs/nextjs.svg",
     },
     {
       id: 6,
-      image: "https://i.imgur.com/oDLp2nF.png",
-      name: "Temps City",
-      description: "Website theme sharing platform.",
-      link: "https://temps.city",
-      language: "PHP",
-      languageIcon: "./assets/techs/php.svg",
+      image: "https://i.imgur.com/fvxBir3.png",
+      name: "Serity",
+      description:
+        "Serity is an interface sales platform. You can buy the interfaces we have prepared for you. Thanks to the licensing system, we keep your interfaces API safe and a 3rd party can never access it. We are here to make sites more secure.",
+      link: "https://serity.me",
+      language: "React",
+      languageIcon: "./assets/techs/react.svg",
     },
   ];
   res.status(200).json(data);
