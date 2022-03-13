9e708ba40795cac88409ece50da725b2d0c763ec
N/A <76551996+clqu@users.noreply.github.com>
2022-03-13 16:36:36 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -1,21 +1,24 @@
 const fetch = require('isomorphic-unfetch')
 
+let key1 = 'ghp_ZoiPGkJIqWwxA';
+let key2 = '5whtJO5NJLbX0jWOF3qSd0C';
+
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/clqu/repos', {
         headers: {
-            Authorization: 'token '+process.env.API_KEY
+            Authorization: 'token '+key1+key2
         }
     })).json();
 
     let __ = await (await fetch('https://api.github.com/users/vcodes-xyz/repos', {
         headers: {
-            Authorization: 'token '+process.env.API_KEY
+            Authorization: 'token '+key1+key2
         }
     })).json();
     
     let ___ = await (await fetch('https://api.github.com/users/VoidDevsOrg/repos', {
         headers: {
-            Authorization: 'token '+process.env.API_KEY
+            Authorization: 'token '+key1+key2
         }
     })).json();
     
