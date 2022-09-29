552628a801a350da377e46a5b8b622d09a51599f
clqu <76551996+clqu@users.noreply.github.com>
2022-09-29 10:19:08 +0300

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,12 +5,12 @@ export default function MyDocument() {
         <Html lang="en">
             <Head>
                 <meta charSet="utf-8" />
-                <meta name="theme-color" content="#47b4fe" />
+                <meta name="theme-color" content="#4F3DFE" />
                 <link href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" rel="stylesheet" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
-                <meta name="description" content="clqu.live | Full-stack Developer" />
+                <meta name="description" content="16 y/o. Full-stack Developer" />
                 <link rel="icon" href="https://www.google.com/favicon.ico" type="image/x-icon"/>
             </Head>
             <body>
